c040e6053dd0e040551efa5234d947306104f30f
hiramonnahareva <hiramonnahaerva@gmail.com>
2024-10-25 14:40:07 +0600

    fix banner image

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,10 @@
+
+
 const LandingPage = () => {
     return (
     <div className="relative">
-        <div className="bg-banner bg-cover p-40 banner text-white relative bg-center flex items-center justify-center bg-gray-200">
-         <div className="absolute z-0 inset-0 bg-black/80"></div>
+        <div className="bg-banner bg-cover p-40 banner text-white relative bg-center flex items-center justify-center bg-gray-200" >
+         <div className="absolute z-0 inset-0 bg-black/80" style={{ "backgroundImage": "url('../assets/banner.jpg')"}}></div>
         <div className="text-center z-10">
           <h1 className="text-6xl font-bold mb-6">Discover Detroit&apos;s Hidden Gems</h1>
           <p className="text-lg text-gray-200 mb-4">Plan Your Epic Weekend with DEW</p>
